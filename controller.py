1f63811a34ab297ba17488de1195d02b8693cfce
R.Berger <r.m.berger@umail.leidenuniv.nl>
2025-04-23 08:12:38 +0200

    Created simulation function. Upgraded some minor other functions (add asset, etc)

--- controller.py
+++ controller.py
@@ -1,46 +1,177 @@
+import matplotlib.pyplot as plt
+import time
+import yfinance as yf
+
 from model import Model
 from view import View
 
-import pandas as pd
-import time
 
 class Controller():
     def __init__(self):
-        pass
+        self.Model = Model()
 
-    def getStarted(self):
+    def GetStarted(self):
         """
         Welcome interface to the user.
         Asks the user which action the user wants to execute
+        Calls designated functions. Only ends when the user
+        enters 'E' or ctrl+C
         """
 
         print("Welcome to the portfolio tracker. What do you want to do today?\n")
-        print("\
+        option = ''
+        valid_options = {'A', 'S', 'V', 'C', 'P'}
+        while (option.capitalize() not in valid_options):
+            print("\
               (A) add assets to your portfolio\n\
               (S) Show the current and historical price of each asset ticker\n\
               (V) View your current portfolio\n\
               (C) Show calculations and weights\n\
-              (P) Perform a simulation\n")
-        time.sleep(1)
-        option = ''
-        valid_options = {'A', 'S', 'V', 'C', 'P'}
-        while (option.capitalize() not in valid_options):
+              (P) Perform a simulation\n\
+              (E) Exit the application\n")
             option = input("Please, enter your option (letter):\n")
             match option.capitalize():
                 case 'A':
-                    print('You chose to add assets to your portfolio')
+                    print('You chose to add assets to your portfolio.\n')
+                    self.NewAsset()
                 case 'S':
-                    print('You chose to show the current and historical price of each asset ticker')
+                    print('You chose to show the current and historical price of each asset ticker.\n')
+                    self.ShowPrices()
                 case 'V':
-                    print('You chose to view your current portfolio')
+                    print('You chose to view your current portfolio.\n')
+                    self.ViewPortfolio()
                 case 'C':
-                    print('You chose to show calculations and weights')
+                    print('You chose to show calculations and weights.\n')
+                    self.ShowCalculations()
                 case 'P':
-                    print('You chose to perform a simulation')
+                    print('You chose to perform a simulation.\n')
+                    self.PerformSimulation()
+                case 'E':
+                    break
                 case _:
-                    print('That is not a valid option. Enter A, S, V, C or P')
+                    print('That is not a valid option. Enter A, S, V, C or P or E (Exit).\n')
+            option = ''
+            time.sleep(1)
+
+
+    def NewAsset(self):
+        """
+        This function adds a ticker to the model. Validates if the ticker exists (yhfinance)
+        and confirms the types of the input (string, etc). Automatically loads asset class
+        and sector from yhfinance.
+        """
+        print("A few questions need to be answered in order to add an asset to your portfolio.")
+        ticker = None
+        while ticker == None:
+            ticker_str = str(input("Please enter a valid ticker: (string)\n"))
+            ticker = self.Model.VerifyTicker(ticker_str)
+            if ticker is not None:
+                break
+
+        print(ticker.info)
+        sector = ticker.info.get('sector')
+        asset_class = ticker.info.get('assetClass')
+        print(f"Sector: {sector}\n")
+        print(f"Asset class: {asset_class}\n")
+
+        quantity = -1
+        while quantity == -1:
+            try:
+                quantity = int(input("Quantity: (int)\n"))
+            except: 
+                print("Please enter an integer")
+        price = -1
+        while price == -1:
+            try:
+                price = float(input("Price: (float)\n"))
+            except:
+                print("Please enter a real number")
+        print(f"Now adding to your portfolio:\n\
+                ticker: {ticker}\n\
+                sector: {sector}\n\
+                class: {asset_class}\n\
+                quantity: {quantity}\n\
+                price: {price}\n")
+        _ = self.Model.AddTicker(ticker, sector, asset_class, quantity, price)
+
+
+    def ShowPrices(self):
+        """
+        This function plots the data of provided tickers. Tickers can be any (not necessary
+        in portfolio). 
+        """
+        print("This function shows the current and historical prices of each ticker.")
+        print("Please, enter the ticker name(s) you want to show. Enter 'C' to continue.")
+        tickers = set()
+        ticker_str = ''
+        while ticker_str.capitalize() != 'C':
+            ticker_str = input("Enter a valid ticker, or typ 'C' to continue:\n")
+            if ticker_str != 'C':
+                if self.Model.VerifyTicker(ticker_str) is not None:
+                    tickers.add(ticker_str)
+                    print(f"Added {ticker_str} to the infograph")
+                else:
+                    print("That is not a valid name. Please enter a valid ticker")
+        if tickers is not None:
+            for ticker in tickers:
+                data = self.Model.GetHistoricalData(ticker_str)
+                price = self.Model.GetPrice(ticker_str)
+                print(price)
+                #Add plots
+        else:
+            print("It seems like you didn't add any valid ticker. Please try again.")
+
+
+    def ViewPortfolio(self):
+        """
+        Shows the assets of the user
+        """
+        print("_______________________________________________________________________________________")
+        print("Ticker | Sector | Class | Quantity | Purchase Price | Transaction Value | Current value")
+        print("_______|________|_______|__________|________________|___________________|______________")
+        for asset in self.Model.GetAssets():
+            print(f"{asset.name} | {asset.sector} | {asset.asset_class} | {asset.quantity} | {asset.purchase_price} | - | {asset.GetPrice()}")
+        print("_______|________|_______|__________|________________|___________________|______________")
+
+
+    def ShowCalculations(self):
+        """
+        Prints calculations for the total portfolio value and the (relative) weights of each asset including
+        the option to see the same per asset class and sector.
+        """
+        print("This function is not implemented yet")
+        print("Would you like to see calculations for the value per:\n\
+             (A) total portfolio\n\
+             (B) asset class\n\
+             (C) asset sector\n")
+        option = ''
+        while option == '':
+            option = input("Please enter your choice (A/B/C):\n")
+            match option:
+                case 'A':
+                    calculations = self.Model.GetCalculations('total')
+                case 'B':
+                    print("Available classes:")
+                    print({asset.asset_class for asset in self.Model.GetAssets()})
+                    asset_class = input("Please enter the asset class you're interested in:\n")
+                    calculations = self.Model.GetCalculations('class', asset_class=asset_class)
+                    print(f"Your portfolio over class {asset_class} is constructed as follows:")
+                case 'C':
+                    print("Available sectors:")
+                    print({asset.sector for asset in self.Model.GetAssets()})
+                    sector = input("Please enter the sector you're interested in:\n")
+                    calculations = self.Model.GetCalculations('sector', sector=sector)
+                    print(f"Your portfolio over sector {sector} is constructed as follows:")
+        print([item for item in calculations])
+
+    def PerformSimulation(self):
+        """
+        Performs a simulation with the current portfolio for upcoming 15 years
+        Includes 100.000 simulations paths
+        """
+        self.Model.GetSimulation(100_000, 15)
 
     
 if __name__ == '__main__':
     control = Controller()
-    control.getStarted()
+    control.GetStarted()
--- model.py
+++ model.py
@@ -1,3 +1,184 @@
+import yfinance as yf
+import numpy as np
+import matplotlib.pyplot as plt
+
+class Asset():
+    def __init__(self, ticker: str, sector: str, asset_class: str, quantity: int, price: float):
+        self.name = ticker
+        self.ticker = yf.Ticker(ticker)
+        self.sector = sector
+        self.asset_class = asset_class
+        self.quantity = quantity
+        self.purchase_price = price
+
+    def GetPrice(self):
+        """
+        Shorthand for if you wish to get the current price an
+        known asset within your portfolio
+        """
+        return self.ticker.info['currentPrice']
+
+
 class Model():
     def __init__(self):
-        pass
+        self.assets = set()
+
+    def VerifyTicker(self, ticker: str):
+        """
+        Checks if the ticker is valid.
+        If there is any recent data on the ticker,
+        this returns true. False otherwise.
+        """
+        try:
+            tick = yf.Ticker(ticker).history(
+                period='1mo')
+            return yf.Ticker(ticker)
+        except:
+            return None # If that failed, it will not exist either 
+
+    def AddTicker(self, ticker: str, sector: str, asset_class: str, quantity: int, price: float):
+        """
+        Given a ticker name (assumed to be validated), sector, asset class, quantity and price
+        This function adds the asset to the user's portfolio
+        """
+        NewAsset = Asset(ticker, sector, asset_class, quantity, price)
+        #with open("portfolio.txt", 'w') as f:
+        #    f.write("ticker")
+        self.assets.add(NewAsset)
+        return NewAsset
+
+    def GetAssets(self):
+        """
+        Returns the set of assets in the portfolio
+        """
+        return self.assets
+    
+    def GetHistoricalData(self, ticker: str, length: str = '1mo'):
+        """
+        Get historical data of the provided ticker (can be any)
+        Returns None if the ticker does not exist
+        Returns pd.dataframe otherwise
+        """
+        try:
+            asset = yf.Ticker(ticker)
+        except:
+            return None
+        historical_data = asset.history(period=length)
+        return historical_data
+    
+    def GetPrice(self, ticker: str):
+        """
+        Get current price of the provided ticker (can be any)
+        Returns none if the ticker does not exist
+        Returns float otherwise
+        """
+        try:
+            asset = yf.Ticker(ticker)
+        except:
+            return None
+        return asset.info['currentPrice']
+    
+    def GetWeight(self, ticker: str, all_assets: set):
+        """
+        Calculates the relative weight of the given asset in the
+        portfolio
+        """
+        total_value = 0
+        for asset in all_assets:
+            if asset.name == ticker:
+                asset_value = asset.quantity * asset.GetPrice()
+            total_value += asset.quantity * asset.GetPrice()
+        return asset_value/total_value
+
+    
+    def GetCalculations(self, option, asset_class = None, sector = None):
+        """
+        Get the components of your portfolio.
+        Dependent on option ('total/class/sector'), assets are selected.
+        Returns a list of dictionaries, where each dictionary contains the
+         - name
+         - weight
+         - value 
+        of selected assets.
+        """
+        calculations = list()
+        if option == 'total':
+            for asset in self.assets:
+                asset_dict = {"ticker": asset.name, "weight": round(self.GetWeight(asset.name, self.assets),3), "value": asset.quantity * asset.GetPrice()}
+                calculations.append(asset_dict)
+        elif option == 'class':
+            for asset in self.assets:
+                if asset.asset_class == asset_class:
+                    asset_dict = {"ticker": asset.name, "weight": round(self.GetWeight(asset.name, self.assets),3), "value": asset.quantity * asset.GetPrice()}
+                    calculations.append(asset_dict)
+        elif option == 'sector':
+            for asset in self.assets:
+                if asset.sector == sector:
+                    asset_dict = {"ticker": asset.name, "weight": round(self.GetWeight(asset.name, self.assets),3), "value": asset.quantity * asset.GetPrice()}
+                    calculations.append(asset_dict)
+        return calculations
+    
+    def GetPortfolioValue(self):
+        """
+        Returns the total portfolio value
+        """
+        total_value = 0
+        for asset in self.assets:
+            total_value += asset.quantity * asset.GetPrice()
+        return total_value
+    
+    def SimulateReturns(self, mean_returns, cov_matrix, num_simulations, time_steps):
+        simulated_returns = np.random.multivariate_normal(
+        mean_returns, cov_matrix, size=(num_simulations, time_steps)
+        ).T 
+        return simulated_returns
+    
+    def GetSimulation(self, num_simulations, num_years):
+        """
+        Loads historical data of provided ticker(s). Then, simulates
+        the behaviour of the tickers and 
+        """
+        initial_portfolio_value = self.GetPortfolioValue()
+        assets = list(self.assets) #So order is preserved
+        weights = [self.getWeight(asset) for asset in assets]
+
+        time_steps = num_years * 252  #Number of trading days * num years
+        portfolio_values = np.zeros((num_simulations, time_steps + 1))
+        portfolio_values[:, 0] = initial_portfolio_value
+        data = yf.download(assets, period='5y') # self.GetHistoryData only accepts strings - this is shorter
+        prices = data['Adj Close']
+        daily_returns = prices.pct_change().dropna()
+        mean_returns = daily_returns.mean()
+        cov_matrix = daily_returns.cov()
+        simulated_daily_returns = self.SimulateReturns(mean_returns, cov_matrix, num_simulations, time_steps)
+        
+        for i in range(num_simulations):
+            for t in range(time_steps):
+                # Calculate portfolio return for this time step
+                asset_returns = simulated_daily_returns[:, t, i]
+                portfolio_return = np.sum(weights * asset_returns)
+                portfolio_values[i, t + 1] = portfolio_values[i, t] * (1 + portfolio_return)
+
+        
+        # 6. Analyze and Visualize Results
+        final_portfolio_values = portfolio_values[:, -1]
+
+        # Histogram of final portfolio values
+        plt.figure(figsize=(10, 6))
+        plt.hist(final_portfolio_values, bins=50, density=True, alpha=0.6, color='skyblue')
+        plt.title('Distribution of Final Portfolio Values (15 Years, 100,000 Simulations)')
+        plt.xlabel('Final Portfolio Value')
+        plt.ylabel('Probability Density')
+        plt.grid(True)
+        plt.show()
+        
+        # Sample of simulated paths
+        plt.figure(figsize=(12, 7))
+        num_paths_to_plot = min(50, num_simulations)
+        for i in np.random.choice(range(num_simulations), num_paths_to_plot, replace=False):
+            plt.plot(portfolio_values[i], alpha=0.1)
+        plt.title('Sample of Simulated Portfolio Paths (15 Years)')
+        plt.xlabel('Time Step (Days)')
+        plt.ylabel('Portfolio Value')
+        plt.grid(True)
+        plt.show()
